9ae0ef6acf06e6b9e2d8620ab151689dc38c07a1
PiyumalT <piyumal-se19035@stu.kln.ac.lk>
2023-09-23 16:10:12 +0530

    Initial commit

---
+++ src/test/java/com/piyumalt/ServiceReservation/ServiceReservationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.piyumalt.ServiceReservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceReservationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
